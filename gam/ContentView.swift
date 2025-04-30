63e4e927321d26d58efa7380cf1d2282b89b231f
Daniel Popovich <pop512413@d204.lths.net>
2025-04-30 12:29:18 -0500

    Initial Commit

---
+++ gam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gam
+//
+//  Created by Popovich, Daniel (512413) on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gam/gamApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamApp.swift
+//  gam
+//
+//  Created by Popovich, Daniel (512413) on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct gamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
