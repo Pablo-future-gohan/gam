f665d6c8d6a5954a23a66a5118586e571f375a0c
Daniel Popovich <pop512413@d204.lths.net>
2025-04-30 12:51:38 -0500

    b

---
+++ gam/GamesView.swift
@@ -0,0 +1,18 @@
+//
+//  GamesView.swift
+//  gam
+//
+//  Created by Popovich, Daniel (512413) on 4/30/25.
+//
+
+import SwiftUI
+
+struct GamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamesView()
+}
